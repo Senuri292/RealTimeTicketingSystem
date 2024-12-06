899da332a85b8e9679bebf627151514612bcfe76
Senuri Hansamini <senurihansamini@gmail.com>
2024-12-06 19:05:48 +0530

    Vendor class completed

--- src/main/java/com/example/RealTimeTicketingSystem/RealTimeTicketingSystemApplication.java
+++ src/main/java/com/example/RealTimeTicketingSystem/RealTimeTicketingSystemApplication.java
@@ -10,24 +10,5 @@ public class RealTimeTicketingSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RealTimeTicketingSystemApplication.class, args);
-
-	}
-
-	public void addVendor(){
-		Scanner input = new Scanner(System.in);
-		System.out.println("------------Vendor Registration------------");
-
-		System.out.print("Enter Vendor's Name: ");
-		String name = input.nextLine();
-
-		System.out.print("Enter Vendor's ID: ");
-		int id = input.nextInt();
-
-		System.out.println("Enter Vendor's contact number: ");
-		int contact = input.nextInt();
-
-		Vendor vendor1 = new Vendor(name, id, contact);
-
-		//Vendor.setVendorDetails.add(new Vendor(vendorName, vendorId, vendorContact));
 	}
 }
